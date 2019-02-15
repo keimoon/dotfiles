cc1f44e86686e658f17ddb12f4b97b43f3ebd165
keimoon <keimoonvie@gmail.com>
2019-02-15 10:12:32 +0700

    Fix HDMI

--- .config/i3/scripts/hdmi.sh
+++ .config/i3/scripts/hdmi.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 if xrandr | grep -q 'HDMI-1 connected '; then
-    exit 0
+    if xrandr | grep -q '+1920'; then
+        exit 0
+    fi
 fi
 xrandr --output HDMI-1 --auto --left-of eDP-1
