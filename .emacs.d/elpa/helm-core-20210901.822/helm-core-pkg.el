54c1f33cb3cd86e6665e531d20dd567b153107e1
keimoon <keimoonvie@gmail.com>
2021-09-06 12:07:44 +0700

    Update emacs packages

---
+++ .emacs.d/elpa/helm-core-20210901.822/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20210901.822" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "07c321a3c7f1676bcf47113f953970a1299ab829")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
